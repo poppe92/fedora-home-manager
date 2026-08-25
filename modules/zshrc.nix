f5938768447d6f5b4d42e4a19a41cddcadfe23b7
Jesper N <jesper.nilsson@omegapoint.se>
2026-08-25 14:39:41 +0200

    add more alisaes for devlopment

--- modules/zshrc.nix
+++ modules/zshrc.nix
@@ -15,7 +15,8 @@
       nvim-tc = "NVIM_APPNAME=\"nvim-typecraft\" nvim";
       recursiveDiskUsage = "du -aBM 2>/dev/null | sort -nr | head -n 50 | more";
       idea = "~/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/bin/idea";
-      nfsResetDbDocker = "docker compose --file $HOME/FRAC/code/fleet-management/nfs-service/nfs-service/docker-compose.yml up -d && docker compose --file $HOME/FRAC/code/fleet-management/nfs-service/nfs-service/docker-compose.yml down && docker volume rm nfs-service_postgres-data && docker compose --file $HOME/FRAC/code/fleet-management/nfs-service/nfs-service/docker-compose.yml up -d";
+      nfsResetDbDocker = "docker compose --file $HOME/FRAC/code/fleet-management/nfs-service/nfs-service/docker-compose.yml up -d && docker compose --file $HOME/FRAC/code/fleet-management/nfs-service/nfs-service/docker-compose.yml down && docker volume rm nfs-service-postgres-data && docker compose --file $HOME/FRAC/code/fleet-management/nfs-service/nfs-service/docker-compose.yml up -d";
+      autosyncResetDbDocker = "docker compose --file $HOME/FRAC/code/fleet-management/autosync-integration/docker-compose.yml up -d && docker compose --file $HOME/FRAC/code/fleet-management/autosync-integration/docker-compose.yml down --volumes && docker compose --file $HOME/FRAC/code/fleet-management/autosync-integration/docker-compose.yml up -d";
       nfsStartLocal = "docker compose --file $HOME/FRAC/code/fleet-management/nfs-service/nfs-service/docker-compose.yml up -d && cd $HOME/FRAC/code/fleet-management/nfs-service && mvn quarkus:dev -pl nfs-service -Dquarkus.profile=local";
     };
     oh-my-zsh = {
